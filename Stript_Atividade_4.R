82c1bb585dba982e60c3670ef21af10b0579e07a
danilosrm <taquaras@gmail.com>
2021-05-11 22:29:01 -0300

    V2
    Verifica existencia do arquivo e faz download caso ele não exista

--- Stript_Atividade_4.R
+++ Stript_Atividade_4.R
@@ -2,6 +2,20 @@
 # Autor: Danilo Sanches
 # Teste de rotina em R
 
+# Verifica se arquivo existe
+# Caso não exista o mesmo será baixado do diretório do Githut
+
+if (file.exists("dados/bugs.txt") == FALSE) {
+  
+  if (dir.exists("dados") == FALSE) {
+    
+    dir.create("dados")
+  }
+  
+  download.file("http://ashipunov.me/data/bugs.txt","dados/bugs.txt")
+  
+}
+
 # Carregando dados para um data.frame
 
 data.f <- read.table("dados/bugs.txt", head=TRUE)
@@ -19,4 +33,7 @@ data.f$COLOR <- factor(data.f$COLOR, labels=c("red", "blue", "green"))
 
 plot(data.f$SEX, data.f$WEIGHT)
 
+
+# Limpa os dados criados durante execução.
+rm(data.f)
 
